9aa6783a209e955fa4d242468ec5511a58bd1540
Victor Taveira Rodrigues <rodrigueseti@outlook.com>
2020-01-04 16:04:48 -0300

    mod linha 9

--- Fibonacci&Recursion.cpp
+++ Fibonacci&Recursion.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 /*
-	Recursividade - uma função que chama ela mesma N vezes
+	Recursividade - uma funÃ§Ã£o que chama ela mesma N vezes
 */
 
 using namespace std;
 
-void fib(int, int a = 0, int b = 1, int c = 1);
+void fib(int, int, int b = 1, int c = 1);
 
 int main(){
 	
-	fib(1134903170); // até nº fib(45) 1134903170, usando int, com o compilador do Dev-C++!
+	fib(1134903170); // atÃ© nÂº fib(45) 1134903170, usando int, com o compilador do Dev-C++!
 	system("start chrome https://oeis.org/A000045/b000045.txt");
 	return 0;
 }
